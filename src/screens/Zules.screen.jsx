e9978a0715f379384f6991bd090b5f32f0b75c15
lohithsrk <srklohith05@gmail.com>
2023-02-23 13:18:33 +0530

    chanegs

--- src/screens/Zules.screen.jsx
+++ src/screens/Zules.screen.jsx
@@ -36,35 +36,27 @@ const Zules = () => {
 	]);
 	const _renderItem = ({ item, index }) => {
 		return (
-			<View>
-				<ImageBackground
-					source={{ uri: item.zuleThumbnail }}
-					style={{ width: windowWidth, height: windowHeight - 1000 }}
-				>
-					<Text>{item.text}</Text>
-					<Text style={{ fontSize: 30 }}>{item.title}</Text>
-				</ImageBackground>
-			</View>
+			<ImageBackground source={{ uri: item.zuleThumbnail }} className='flex-1'>
+				<Text>{item.text}</Text>
+				<Text style={{ fontSize: 30 }}>{item.title}</Text>
+			</ImageBackground>
 		);
 	};
 	return (
 		<View className='bg-black flex-1'>
-			{/* <View className='border-b-2 border-gray-300 p-3 pb-2'>
+			<View className='border-b-2 border-gray-300 p-3 pb-2'>
 				<Image source={require('../assets/logo.png')} className='w-36 h-8' />
 			</View>
-			<View className='p-3'>
+			<View className='p-3 flex-1'>
 				<Carousel
 					ref={(ref) => (carousel = ref)}
 					data={carouselItems}
 					sliderWidth={windowWidth - 24}
-					itemWidth={windowWidth - 24}
-					sliderHeight={windowHeight - 100}
-					itemHeight={windowHeight - 100}
 					renderItem={_renderItem}
 					onSnapToItem={(index) => setActiveIndex(index)}
 					layout={'tinder'}
 				/>
-			</View> */}
+			</View>
 		</View>
 	);
 };
